674c4e5ffd03d2af5c65148343e6d1dd8e379065
davidmarin25 <davidmarinospina@gmail.com>
2019-03-05 06:35:33 -0500

    dfd

--- app/src/main/java/com/example/a312c/androiddatabasemarcelo/MainActivity.java
+++ app/src/main/java/com/example/a312c/androiddatabasemarcelo/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends Activity {
         et4 = (EditText) findViewById(R.id.editText4);
     }
 
+    
     /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
